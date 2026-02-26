5a9fbbc4bfc4b322b1fb3d2e1a8257634be3b3bd
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-26 10:58:42 +0600

    explored the var, const, let usage rules

---
+++ var-let-const.js
@@ -0,0 +1,8 @@
+const name = "John Doe"; // This variable cannot be reassigned
+name = "Jane Doe"; // This will throw an error
+
+let age = 30; // This variable can be reassigned
+age = 31; // This is allowed
+
+var city = "New York"; // This variable can be reassigned and has function scope
+city = "Los Angeles"; // This is allowed
