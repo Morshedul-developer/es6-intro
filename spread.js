fc073cc39591c76253219e843cd58fa2722f6413
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-26 12:03:04 +0600

    explored about spread operator

---
+++ spread.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4, 5];
+console.log(newNumbers); // Output: [1, 2, 3, 4, 5]
+
+const person = { name: 'Alice', age: 30 };
+const newPerson = { ...person, city: 'New York' };
+person.age = 31; // Modifying the original object does not affect the new object
+
+console.log(person); // Output: { name: 'Alice', age: 31 }
+console.log(newPerson); // Output: { name: 'Alice', age: 30, city: 'New York' }
