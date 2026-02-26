415a4bed31bdb8934e84f4b8efe9f25a3abcc7ca
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-26 12:04:48 +0600

    made a little change to the person object

--- spread.js
+++ spread.js
@@ -4,7 +4,7 @@ console.log(newNumbers); // Output: [1, 2, 3, 4, 5]
 
 const person = { name: 'Alice', age: 30 };
 const newPerson = { ...person, city: 'New York' };
-person.age = 31; // Modifying the original object does not affect the new object
+person.isMarried = true; // Modifying the original object does not affect the new object
 
-console.log(person); // Output: { name: 'Alice', age: 31 }
+console.log(person); // Output: { name: 'Alice', age: 31, isMarried: true }
 console.log(newPerson); // Output: { name: 'Alice', age: 30, city: 'New York' }
